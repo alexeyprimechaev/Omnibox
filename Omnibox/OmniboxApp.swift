af1265619870aa66491379f8a3458fe4c0b2380d
LEKS <leks@lekskeks.com>
2022-08-24 00:09:46 +0300

    Initial Commit

---
+++ Omnibox/OmniboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  OmniboxApp.swift
+//  Omnibox
+//
+//  Created by Alexey Primechaev on 8/24/22.
+//
+
+import SwiftUI
+
+@main
+struct OmniboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
